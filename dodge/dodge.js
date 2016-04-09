ff94b820145ab4202f5b9971ca7c06a20d664548
Joshua Bumgarner <bobulousness@gmail.com>
2016-04-09 11:46:47 -0400

    Update dodge.js

--- dodge/dodge.js
+++ dodge/dodge.js
@@ -57,7 +57,7 @@ Dodge.Play.prototype = {
      
     }
     //movement towards player
-    if (this.obj.velocity.x > this.player.velocity.x) {
+    if (this.obj.x > this.player.x) {
       this.obj.velocity.x -= 1;
     } 
     if (this.obj.velocity.x < this.player.velocity.x) {
