cc354b10e3952709cf7a8889d59ad9351906563a
Joshua Bumgarner <bobulousness@gmail.com>
2016-04-23 10:25:02 -0400

    Update dodgetest.js

--- test/dodgetest.js
+++ test/dodgetest.js
@@ -90,5 +90,7 @@ Dodge.Play.prototype = {
 };
 //----------------------------------------------------------------------------------------
 var game = new Phaser.Game(320,568);
+game.state.add('title', Dodge.Title);
 game.state.add('play', Dodge.Play);
 game.state.start('play');
+
