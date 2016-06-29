8ba7ede6ca55ff52de16aaec303e5ddfba55380d
Joshua Bumgarner <bobulousness@gmail.com>
2016-06-29 19:39:56 -0400

    Create killme.js

---
+++ project/killme.js
@@ -0,0 +1,19 @@
+'use strict';
+
+killme = {}
+killme.Play = function () {}
+
+  killme.Play.prototype = {
+  init: function() {
+    //creating variables
+  }
+  preload: funtction() {
+  //loading images
+  }
+  create: function() {
+  //creating the game scene
+  }
+  update: function() {
+  // Game logic
+  }
+}
