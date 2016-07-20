ae6604228bf931e018b357fa0c69f7644d161897
Joshua Bumgarner <bobulousness@gmail.com>
2016-07-20 18:41:17 -0400

    Update killme.js

--- project/killme.js
+++ project/killme.js
@@ -7,11 +7,18 @@ killme.Play = function () {}
   init: function() {
     //creating variables
   }
-  preload: funtction() {
+  preload: function() {
     //loading images
+    this.load.image('bckgr','strsky.gif');
+    this.load.image('blck','blck.gif');
+    this.load.image('sqhd','sqhd.gif');
   }
   create: function() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    
     cursors = game.input.keyboard.createCursorKeys();
+    //background
+    this.bckgr = this.add.tileSprite(1,1,800,600,'bckgr')
     
     //creating the game scene
   }
