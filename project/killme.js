2461e66eab8b7a7a36aa8f234cde41121d5899a4
Joshua Bumgarner <bobulousness@gmail.com>
2016-07-20 19:53:07 -0400

    Update killme.js

--- project/killme.js
+++ project/killme.js
@@ -8,7 +8,7 @@ killme.Play.prototype = {
   }
   preload: function() {
     //loading images
-    this.load.spritesheet('bckgr', 'strsky.png', 800, 600, 10);
+    this.load.spritesheet('bckgr', 'strsky.png', 800, 600, 1);
     this.load.image('blck1', 'blck.png');
     this.load.image('sqhd', 'sqhd.png');
   }
@@ -16,8 +16,7 @@ killme.Play.prototype = {
     
     cursors = game.input.keyboard.createCursorKeys();
     //background
-    this.background = this.add.tileSprite(1,1,800,600,'bckgr');
-    this.background.autoScroll(0,0);
+    this.background = this.add.sprite(0,0,800,600,'bckgr');
     this.background.scale.set(1);
    
     
