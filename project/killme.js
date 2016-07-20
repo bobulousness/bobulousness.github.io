7073686bbe5d5aa507eb522d8c27d32a37516128
Joshua Bumgarner <bobulousness@gmail.com>
2016-07-20 19:44:05 -0400

    Update killme.js

--- project/killme.js
+++ project/killme.js
@@ -16,7 +16,9 @@ killme.Play.prototype = {
     
     cursors = game.input.keyboard.createCursorKeys();
     //background
-    this.bckgr = this.add.sprite(800,600,'bckgr');
+    this.background = this.add.tileSprite(1,1,800,600,'bckgr');
+    this.background.autoScroll(0,0);
+    this.background.scale.set(1);
    
     
     //block
