85b5edd8d4bbc99ad4288a3c82e8635a7ebc4e4c
Joshua Bumgarner <bobulousness@gmail.com>
2016-07-20 20:59:38 -0400

    Update killme.js

--- project/killme.js
+++ project/killme.js
@@ -13,11 +13,7 @@ killme.Play.prototype = {
     this.load.image('sqhd', 'sqhd.png');
   },
   
-  hort: function(x, y) {
-    this.blck = this.add.sprite(x, y, 'blck1');
-    this.blck1 = this.add.sprite(x += 32, y, 'blck1');
-    this.blck2 = this.add.sprite(x += 64, y, 'blck1');
-  },
+
   create: function() {
     
     this.cursors = game.input.keyboard.createCursorKeys();
@@ -25,6 +21,11 @@ killme.Play.prototype = {
     this.background = this.add.tileSprite(1,1,800,600,'bckgr', 10);
     
     //block
+    hort: function(x, y) {
+      this.blck = this.add.sprite(x, y, 'blck1');
+      this.blck1 = this.add.sprite(x += 32, y, 'blck1');
+      this.blck2 = this.add.sprite(x += 64, y, 'blck1');
+  },
     hort(500,360);
     this.blck.anchor.setTo(0.5, 0.5);
     game.physics.arcade.enable(this.obj);
