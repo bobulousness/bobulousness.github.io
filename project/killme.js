27a8acb971caa5a9aefce77b70a3e1ca0fa2637c
Joshua Bumgarner <bobulousness@gmail.com>
2016-07-27 18:27:26 -0400

    Update killme.js

--- project/killme.js
+++ project/killme.js
@@ -13,7 +13,7 @@ class PlayState {
   
 
   create() {
-    hort: function(x, y) {
+    hort(x, y) {
       this.blck = this.add.sprite(x, y, 'blck1');
       this.blck1 = this.add.sprite(x += 32, y, 'blck1');
       this.blck2 = this.add.sprite(x += 64, y, 'blck1');
