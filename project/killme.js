98b83411a1e70004105895633fc3bdce44f05331
Joshua Bumgarner <bobulousness@gmail.com>
2016-07-27 18:30:27 -0400

    Update killme.js

--- project/killme.js
+++ project/killme.js
@@ -11,10 +11,10 @@ class PlayState {
     this.load.image('sqhd', 'sqhd.png');
   }
       //functionm for horizontal block placement
-    hort(x, y) {
-      this.blck = this.add.sprite(x, y, 'blck1');
-      this.blck1 = this.add.sprite(x += 32, y, 'blck1');
-      this.blck2 = this.add.sprite(x += 64, y, 'blck1');
+  var hort = hort(x, y) {
+    this.blck = this.add.sprite(x, y, 'blck1');
+    this.blck1 = this.add.sprite(x += 32, y, 'blck1');
+    this.blck2 = this.add.sprite(x += 64, y, 'blck1');
   }
 
   create() {
