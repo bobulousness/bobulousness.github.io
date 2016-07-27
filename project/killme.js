53c078b6c97993332a1591464aaffc4075899766
Joshua Bumgarner <bobulousness@gmail.com>
2016-07-27 18:22:06 -0400

    Update killme.js

--- project/killme.js
+++ project/killme.js
@@ -3,13 +3,13 @@ class PlayState {
 
   init() {
     //creating variables
-  },
+  }
   preload() {
     //loading images
     this.load.spritesheet('bckgr', 'strsky.png', 800, 600, 10);
     this.load.image('blck1', 'blck.png');
     this.load.image('sqhd', 'sqhd.png');
-  },
+  }
   
 
   create() {
@@ -17,7 +17,7 @@ class PlayState {
       this.blck = this.add.sprite(x, y, 'blck1');
       this.blck1 = this.add.sprite(x += 32, y, 'blck1');
       this.blck2 = this.add.sprite(x += 64, y, 'blck1');
-  },*/
+  }*/
     
     this.cursors = game.input.keyboard.createCursorKeys();
     //background
@@ -30,10 +30,10 @@ class PlayState {
     
     game.physics.arcade.enable(this.blck);
     //creating the game scene
-  },
+  }
   update() {
     // Game logic
-  },
+  }
 }
 var game = new Phaser.Game(800,600);
 game.state.add('Play', PlayState);
