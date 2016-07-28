1eada8f349e1d1514851245c1fe86d2688e49054
Joshua Bumgarner <bobulousness@gmail.com>
2016-07-27 20:31:02 -0400

    Update killme.js

--- project/killme.js
+++ project/killme.js
@@ -10,6 +10,8 @@ class PlayState {
     this.load.spritesheet('bckgr', 'strsky.png', 800, 600, 10);
     this.load.image('blck', 'blck.png');
     this.load.image('sqhd', 'sqhd.png');
+    this.load.image('stpvrt','blckstpvrt.png');
+    this.load.image('stphrt','blckstphrt.png');
   }
       //functionm for horizontal block placement
 
