2aa126da17f235046fc3e79fa6aff8c2176c3203
Joshua Bumgarner <bobulousness@gmail.com>
2016-08-17 19:48:43 -0400

    Update kmp.js

--- project/kmp.js
+++ project/kmp.js
@@ -42,7 +42,7 @@ vert(x, y) {
     this.player = this.add.sprite(400,500, 'sqhd');
     game.physics.arcade.enable(this.player);
     this.player.body.collideWorldBounds = true;
-    this.player.body.bounce.setTo(.03);
+    this.player.body.bounce.setTo(1);
     this.player.body.drag.setTo(30);
     this.player.body.gravity.set(0,180);
     //block
