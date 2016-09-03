369295f378389d0e6574d3096cd17291fb37c107
Joshua Bumgarner <bobulousness@gmail.com>
2016-09-03 11:39:14 -0400

    Delete shooter.js

--- shooter/shooter.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-
------------------------------------------------------------------------------------------
-var game = new Phaser.Game(320,568);
-game.state.add('play', Dodge.Play);
-game.state.start('play');
